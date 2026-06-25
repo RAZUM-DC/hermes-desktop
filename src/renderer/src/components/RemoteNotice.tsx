bf5210e3f602aa437eed58701b80834c513b09de
eNamre <erman@razum.life>
2026-06-25 18:11:45 +0400

    i18n: RemoteNotice RU translation (РАЗУМ remote notice)

--- src/renderer/src/components/RemoteNotice.tsx
+++ src/renderer/src/components/RemoteNotice.tsx
@@ -4,10 +4,10 @@ function RemoteNotice({ feature }: { feature: string }): React.JSX.Element {
   return (
     <div className="remote-notice">
       <Signal size={28} className="remote-notice-icon" />
-      <p className="remote-notice-title">Connected to remote Hermes</p>
+      <p className="remote-notice-title">Подключено к серверу РАЗУМ</p>
       <p className="remote-notice-desc">
-        {feature} is not available in remote mode. This data lives on the server
-        and is not accessible through the API yet.
+        {feature} недоступно в удалённом режиме. Эти данные живут на сервере и
+        пока не доступны через API.
       </p>
     </div>
   );
